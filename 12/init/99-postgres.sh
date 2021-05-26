7e354eb07717e11bd29e75b663484c9ba410a4af
Alexey U. Maksutov <alexey.maksutov@gmail.com>
2021-05-26 14:36:16 -0400

    Version 12 added

---
+++ 12/init/99-postgres.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+test -f /var/lib/postgresql/PG_VERSION || sudo -u "$PUSER" -g "$PGROUP" initdb -D/var/lib/postgresql --auth-local=trust --auth-host=md5 --no-locale --pwfile=/etc/postgresql/pwfile
+
+exec sudo -u "$PUSER" -g "$PGROUP" postgres -i -D/var/lib/postgresql
